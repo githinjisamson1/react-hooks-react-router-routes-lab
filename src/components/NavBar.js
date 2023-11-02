a9e26e70bcafd7cb1a1b0aedfa67bd2cc36e244b
Samson Githinji <githinjisamson148@gmail.com>
2023-11-02 07:17:56 +0300

    Completed assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,35 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  // const navItems = ["Home", "Movies", "Directors", "Actors"];
+  return (
+    <div className="navbar">
+      {/* <ul>
+        {navItems.map((navItem) => {
+          return (
+            <li key={navItem}>
+              <NavLink to={`/${navItem}`}>{navItem}</NavLink>
+            </li>
+          );
+        })}
+      </ul> */}
+      
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/movies">Movies</NavLink>
+        </li>
+        <li>
+          <NavLink to="/directors">Directors</NavLink>
+        </li>
+        <li>
+          <NavLink to="/actors">Actors</NavLink>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default NavBar;
